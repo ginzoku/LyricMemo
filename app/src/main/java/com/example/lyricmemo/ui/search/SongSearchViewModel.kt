3dc28dadf8a4465c7ee93348f7e27b9526ea7fe5
ginzoku <minaminoharukaze@yahoo.co.jp>
2025-12-09 04:23:28 +0900

    ・SearchType関連不具合修正

--- app/src/main/java/com/example/lyricmemo/ui/search/SongSearchViewModel.kt
+++ app/src/main/java/com/example/lyricmemo/ui/search/SongSearchViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
